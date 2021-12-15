d95a4deac94a04ebffdf22823a9294ba8b0954f1
Arthur Suermondt <arthuracs@gmail.com>
2021-12-15 16:28:14 +0100

    Wrap all jQuery calls in document ready

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
-// grab query params
-const params = new URLSearchParams(window.location.search)
-
-// initialize the chat log to give GPT-3 context on the conversation
-let currentChatLog = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n";
-
-// If a valid OpenAI API key was provided, start the chat and seed the chat log
-const apiKey = params.has('key') ? params.get('key') : false;
-if (!apiKey) {
-    $('input[name="input-text"]').prop('disabled', true);
-    addMessage('You must provide an OpenAI API key as ?key=[API KEY].');
-} else {
-    let initialMessageSent = 'Hello, how are you?';
-    let initialMessageReceived = 'I am doing great. How can I help you today?';
-
-    addMessage(initialMessageSent, true);
-    addMessage(initialMessageReceived,false);
-    appendInteractionToChatlog(initialMessageSent, initialMessageReceived, currentChatLog);
-}
-
-// add a new message to the page with a date/time stamp, either from the user or a computer response
-function addMessage(message, sent) {
-    const dateStyle = {dateStyle: 'medium', timeStyle: 'medium'};
-    const messageDate = new Date();
-    const formattedDate = messageDate.toLocaleString(undefined, dateStyle);
-
-    $('#messages').append(`<div class='message ${sent ? 'sent' : 'received'}'>
-        <div class='message-text'>${message}</div>
-        <div class='timestamp'>${formattedDate}</div></div>`);
-
-}
-
-// save interaction to the chat log to make sure GPT-3 continues to have context on the full conversation
-// this probably breaks at some point if the chat log becomes very large
-function appendInteractionToChatlog(question, answer, chatLog) {
-    currentChatLog = `${currentChatLog}Human: ${question}\nAI: ${answer}\n`;
-}
-
-// show 'typing' style animation while the user waits for the computer to respond
-function startWaiting() {
-    $('#messages').append("<div class=\"message waiting\"><div class=\"dot-flashing\"></div></div>");
-}
-
-// remove 'typing' style animation when the computer is ready to respond
-function stopWaiting() {
-    $('.waiting').remove();
-}
-
-
 $( document ).ready(function() {
+
+    // grab query params
+    const params = new URLSearchParams(window.location.search)
+
+    // initialize the chat log to give GPT-3 context on the conversation
+    let currentChatLog = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n";
+
+    // If a valid OpenAI API key was provided, start the chat and seed the chat log
+    const apiKey = params.has('key') ? params.get('key') : false;
+    if (!apiKey) {
+        $('input[name="input-text"]').prop('disabled', true);
+        addMessage('You must provide an OpenAI API key as ?key=[API KEY].');
+    } else {
+        let initialMessageSent = 'Hello, how are you?';
+        let initialMessageReceived = 'I am doing great. How can I help you today?';
+
+        addMessage(initialMessageSent, true);
+        addMessage(initialMessageReceived,false);
+        appendInteractionToChatlog(initialMessageSent, initialMessageReceived, currentChatLog);
+    }
+
+    // add a new message to the page with a date/time stamp, either from the user or a computer response
+    function addMessage(message, sent) {
+        const dateStyle = {dateStyle: 'medium', timeStyle: 'medium'};
+        const messageDate = new Date();
+        const formattedDate = messageDate.toLocaleString(undefined, dateStyle);
+
+        $('#messages').append(`<div class='message ${sent ? 'sent' : 'received'}'>
+            <div class='message-text'>${message}</div>
+            <div class='timestamp'>${formattedDate}</div></div>`);
+
+    }
+
+    // save interaction to the chat log to make sure GPT-3 continues to have context on the full conversation
+    // this probably breaks at some point if the chat log becomes very large
+    function appendInteractionToChatlog(question, answer, chatLog) {
+        currentChatLog = `${currentChatLog}Human: ${question}\nAI: ${answer}\n`;
+    }
+
+    // show 'typing' style animation while the user waits for the computer to respond
+    function startWaiting() {
+        $('#messages').append("<div class=\"message waiting\"><div class=\"dot-flashing\"></div></div>");
+    }
+
+    // remove 'typing' style animation when the computer is ready to respond
+    function stopWaiting() {
+        $('.waiting').remove();
+    }
 
     // handle text submission from the user
     $('#input-message-form').on('submit', function(event) {
